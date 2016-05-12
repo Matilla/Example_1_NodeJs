8c520cc3f6e4121ea012a398856eb7ba4198e368
Luis Matilla <luis.matilla@gmail.com>
2016-05-12 15:36:42 +0000

    segundo commit

--- example_1_nodejs_http_server.js
+++ example_1_nodejs_http_server.js
@@ -3,8 +3,8 @@ const http = require('http');
 //const hostname = 'matilla-examples_node_js-3151490';
 //const port = 3000;
 
-const hostname = process.env.IP;
-const port = process.env.PORT;
+const hostname = process.env.IP; // esto "process.env.IP" parece propio de cloud9, no se puede hacer el push a bluemix asi porque no lo entendera
+const port = process.env.PORT;  // esto "process.env.PORT" parece propio de cloud9, no se puede hacer el push a bluemix asi porque no lo entendera
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
